c49fd79b60be394a6c4643c745dda0a4656726c4
popy-jopy49 <70320407+popy-jopy49@users.noreply.github.com>
2023-06-08 09:40:38 +1200

    Update Wrench.cs

--- MechanicalMayhem/Assets/Scripts/Wrench.cs
+++ MechanicalMayhem/Assets/Scripts/Wrench.cs
@@ -3,6 +3,30 @@ using UnityEngine;
 public class Wrench : MonoBehaviour
 {
 
+    [SerializeField] private float fireRate;
+    [SerializeField] private float damage;
+    [SerializeField] private float range;
+    [SerializeField] private LayerMask whatToHit;
+    private float time;
 
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (time < fireRate)
+        {
+            time += Time.deltaTime;
+            return;
+        }
+
+        time = 0;
+        /*if (Physics2D.Raycast(transform.position, transform.up, range, whatToHit, ))
+        {
+
+        }*/
+    }
 
 }
