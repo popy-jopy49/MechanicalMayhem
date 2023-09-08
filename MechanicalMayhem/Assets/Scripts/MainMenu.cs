4aa8773cec4099b9c710b33f6c91d85897a74cae
TomS274 <128107938+TomS274@users.noreply.github.com>
2023-09-08 15:06:26 +1200

    pp

--- MechanicalMayhem/Assets/Scripts/MainMenu.cs
+++ MechanicalMayhem/Assets/Scripts/MainMenu.cs
@@ -6,9 +6,12 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame() 
-    {
+    { 
         SceneManager.LoadSceneAsync(1);
     }
 
-
+    public void QuitGame()
+    {
+        Application.Quit();      
+    }
 }
